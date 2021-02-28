ad5762db3887e65d6925f2060b3c899ad15a2671
Shahil Shah <shahilushah@gmail.com>
2021-02-28 13:10:51 -0500

    Added docstrings

--- code/main.py
+++ code/main.py
@@ -13,42 +13,88 @@ from keras import (
 from keras_applications.resnext import ResNeXt101
 import time
 from efficientnet.keras import EfficientNetB6
-import json
 from faceCrops import GenerateFaceCrops
 import argparse
 
 
 class Result:
-    def generate_classifications(self, probability_scores, threshold):
+    """
+    The result class is resposible for generating probability scores for the
+    ensemble and for generating final classifications.
+    """
+
+    def generate_classifications(self,
+                                 probability_scores,
+                                 threshold):
+        """
+        This functions is used to generate classification
+        into deepfake and real images.
+
+        Args:
+            probability_scores (dict of str: float): Mapping of
+                                                     video name to probability
+            threshold (float): Value above which deepfake
+
+        Returns:
+            list[int]: list having classifications for each video.
+                       1- deepfake, 0- real.
+        """
         y_pred = []
 
-        for keys in probability_scores.keys():
-            if probability_scores[keys] > threshold:
+        for key in probability_scores:
+            if probability_scores[key] > threshold:
                 y_pred.append(1)
             else:
                 y_pred.append(0)
         return(y_pred)
 
-    def ensemble(self, scores_resnext, scores_efficient):
+    def ensemble(self,
+                 scores_resnext,
+                 scores_efficient):
+        """
+        This function is used to get ensemble probabilities
+        from individual model probabillities.
+
+        Args:
+            scores_resnext (dict of str: float): Mapping of video name to
+                                                 probability for resnext model.
+            scores_efficient (dict of str: float): Mapping of video name
+                                                   to probability
+                                                   for efficientnet.
+
+        Returns:
+            dict of 'str: float': Mapping of video name to
+                                  probability for ensemble model.
+        """
         ensembled = {}
-        for keys in scores_resnext.keys():
-            ensembled[keys] = (0.5 * scores_efficient[keys]
-                               + 0.5 * scores_resnext[keys])
+        for key in scores_resnext:
+            ensembled[key] = (0.5 * scores_efficient[key]
+                              + 0.5 * scores_resnext[key])
         return ensembled
-
-    def dump_json(self, file_name, dictionary):
-        with open(file_name + ".json", "w") as fp:
-            json.dump(dictionary, fp)
-
-    def read_json(self, file_name):
-        with open(file_name + ".json") as f:
-            scores1 = json.load(f)
-        return scores1
 
 
 class ResNext:
-    def __load_model(self, learning_rate=0, trainable_layers=0):
-        print("MODEL LOADING..........")
+    """
+    The ResNext class is responsible for training a ResNext
+    model and predicting if a video is deepfake or not.
+    """
+
+    def __load_model(self,
+                     learning_rate,
+                     trainable_layers):
+        """
+        This private function creates a Model object with the
+        base model as ResNext101.
+
+        Args:
+            learning_rate (float): Defines the learning rate
+                                   to train the model.
+            trainable_layers (int): Defines the number of
+                                    trainable layers from bottom.
+
+        Returns:
+           keras.models.Model() : ResNext model object.
+        """
         in_lay = keras_layers.Input(shape=(224, 224, 3))
         base_model = model = ResNeXt101(
             input_tensor=keras_layers.Input(shape=(224, 224, 3)),
@@ -83,7 +129,20 @@ class ResNext:
               batch_size,
               learning_rate,
               trainable_layers):
+        """
+        This function trains the resnext model.
 
+        Args:
+            train_set_dir (str): Path to the folder having training images.
+            epochs (int): Variable representing the
+                          number of epochs for training.
+            batch_size (int): Variable representing
+                              the batch size to be used for trianing.
+            learning_rate (int, float): Defines the learning rate
+                                   to train the model.
+            trainable_layers (int): Defines the number of
+                                    trainable layers from bottom.
+        """
         train_datagen = image.ImageDataGenerator(rescale=1.0 / 255)
         train_generator = train_datagen.flow_from_directory(
             train_set_dir,
@@ -101,7 +160,20 @@ class ResNext:
 
         model.save_weights("Weights/ResnextDeepFakes.h5")
 
-    def __predict_images(self, test_set_dir):
+    def __predict_images(self,
+                         test_set_dir):
+        """
+        This private function gives probability scores using the
+        face crops generated using predict_videos() function.
+
+        Args:
+            test_set_dir (str): Path to the directory
+                                where facecrops for each video is generated.
+
+        Returns:
+            dict of 'str: float': Mapping of video name to
+                                  probability for ensemble model.
+        """
         model = self.__load_model()
         print("Model has been loaded")
         model.load_weights("Weights/ResnextDeepFakes.h5")
@@ -149,15 +221,39 @@ class ResNext:
 
         return probability_scores
 
-    def predict_videos(self, test_set_dir, frames):
+    def predict_videos(self,
+                       test_set_dir,
+                       frames):
+        """
+        This function is used to classify videos.
+
+        Args:
+            test_set_dir (str): Path to the directory of videos.
+            frames (int): Variable representing the number of
+                          frames to be selected from the videos.
+        """
         face_crop_object = GenerateFaceCrops(test_set_dir, frames)
         face_crop_object.face_crops(test_set_dir+"/FaceCrops")
         return(self.__predict_images(test_set_dir+"/FaceCrops"))
 
 
 class EfficientNetWithAttention:
-    def __load_model(self, learning_rate=0, trainable_layers=0):
-        print("MODEL LOADING..........")
+    def __load_model(self,
+                     learning_rate,
+                     trainable_layers):
+        """
+        This private function creates a Model object with the
+        base model as EfficientNetB6.
+
+        Args:
+            learning_rate (float): Defines the learning rate
+                                   to train the model.
+            trainable_layers (int): Defines the number of
+                                    trainable layers from bottom.
+
+        Returns:
+           keras.models.Model() : EfficientNet model object.
+        """
         in_lay = keras_layers.Input(shape=(224, 224, 3))
         base_model = EfficientNetB6(
             input_shape=(224, 224, 3), weights="imagenet", include_top=False
@@ -166,7 +262,6 @@ class EfficientNetWithAttention:
         pt_features = base_model(in_lay)
         pt_depth = base_model.get_output_shape_at(0)[-1]
         bn_features = keras_layers.BatchNormalization()(pt_features)
-        # here we do an attention mechanism to turn pixels in the GAP on an off
         attn_layer = keras_layers.Conv2D(
             64, kernel_size=(1, 1), padding="same", activation="relu"
         )(keras_layers.Dropout(0.5)(bn_features))
@@ -179,7 +274,6 @@ class EfficientNetWithAttention:
         attn_layer = keras_layers.Conv2D(
             1, kernel_size=(1, 1), padding="valid", activation="sigmoid"
         )(attn_layer)
-        # fan it out to all of the channels
         up_c2_w = np.ones((1, 1, 1, pt_depth))
         up_c2 = keras_layers.Conv2D(
             pt_depth,
@@ -195,7 +289,6 @@ class EfficientNetWithAttention:
         mask_features = keras_layers.multiply([attn_layer, bn_features])
         gap_features = keras_layers.GlobalAveragePooling2D()(mask_features)
         gap_mask = keras_layers.GlobalAveragePooling2D()(attn_layer)
-        # to account for missing values from the attention model
         gap = keras_layers.Lambda(lambda x: x[0] / x[1], name="RescaleGAP")(
             [gap_features, gap_mask]
         )
@@ -222,7 +315,20 @@ class EfficientNetWithAttention:
               batch_size,
               learning_rate,
               trainable_layers):
+        """
+        This function trains the efficientnet model.
 
+        Args:
+            train_set_dir (str): Path to the folder having training images.
+            epochs (int): Variable representing the
+                          number of epochs for training.
+            batch_size (int): Variable representing
+                              the batch size to be used for trianing.
+            learning_rate (int, float): Defines the learning rate
+                                   to train the model.
+            trainable_layers (int): Defines the number of
+                                    trainable layers from bottom.
+        """
         train_datagen = image.ImageDataGenerator(rescale=1.0 / 255)
         train_generator = train_datagen.flow_from_directory(
             train_set_dir,
@@ -240,7 +346,20 @@ class EfficientNetWithAttention:
 
         model.save_weights("Weights/EFB6withAttentionDeepFakes.h5")
 
-    def __predict_images(self, test_set_dir):
+    def __predict_images(self,
+                         test_set_dir):
+        """
+        This private function gives probability scores using the
+        face crops generated using predict_videos() function.
+
+        Args:
+            test_set_dir (str): Path to the directory
+                                where facecrops for each video is generated.
+
+        Returns:
+            dict of 'str: float': Mapping of video name to
+                                  probability for ensemble model.
+        """
         model = self.__load_model()
         print("Model has been loaded")
         model.load_weights("Weights/EFB6withAttentionDeepFakes.h5")
@@ -288,7 +407,17 @@ class EfficientNetWithAttention:
 
         return probability_scores
 
-    def predict_videos(self, test_set_dir, frames):
+    def predict_videos(self,
+                       test_set_dir,
+                       frames):
+        """
+        This function is used to classify videos.
+
+        Args:
+            test_set_dir (str): Path to the directory of videos.
+            frames (int): Variable representing the number of
+                          frames to be selected from the videos.
+        """
         face_crop_object = GenerateFaceCrops(test_set_dir, frames)
         face_crop_object.face_crops(test_set_dir+"/FaceCrops")
         return(self.__predict_images(test_set_dir+"/FaceCrops"))
@@ -297,22 +426,51 @@ class EfficientNetWithAttention:
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--genFaceCrops", action="store_true")
-    parser.add_argument("--frames", nargs="?", default=30, type=int)
-    parser.add_argument("--videoDirectory", nargs="?", default="test_videos")
 
-    parser.add_argument("--trainEfficientNet", action="store_true")
-    parser.add_argument("--trainResNext", action="store_true")
-    parser.add_argument("--epochs", nargs="?", default=200, type=int)
-    parser.add_argument("--batchSize", nargs="?", default=64, type=int)
-    parser.add_argument("--imageDirectory", nargs="?", default="train_images")
+    parser.add_argument("--genFaceCrops",
+                        action="store_true")
+
+    parser.add_argument("--frames",
+                        nargs="?",
+                        default=30,
+                        type=int)
+
+    parser.add_argument("--videoDirectory",
+                        nargs="?",
+                        default="test_videos")
+
+    parser.add_argument("--trainEfficientNet",
+                        action="store_true")
+    parser.add_argument("--trainResNext",
+                        action="store_true")
+
+    parser.add_argument("--epochs",
+                        nargs="?",
+                        default=200,
+                        type=int)
+
+    parser.add_argument("--batchSize",
+                        nargs="?",
+                        default=64,
+                        type=int)
+
+    parser.add_argument("--imageDirectory",
+                        nargs="?",
+                        default="train_images")
+
     parser.add_argument("--learningRate",
                         nargs="?",
                         default=0.0001,
                         type=float)
-    parser.add_argument("--trainableLayers", nargs="?", default=5, type=int)
 
-    parser.add_argument("--test", action="store_true")
+    parser.add_argument("--trainableLayers",
+                        nargs="?",
+                        default=5,
+                        type=int)
+
+    parser.add_argument("--test",
+                        action="store_true")
+
     parser.add_argument("--testVideoDirectory",
                         nargs="?",
                         default="test",
@@ -325,7 +483,6 @@ if __name__ == "__main__":
         face_crop_object.face_crops()
 
     elif args.trainResNext:
-        print(args)
         resnext = ResNext()
         resnext.train(
             args.imageDirectory,
@@ -334,6 +491,7 @@ if __name__ == "__main__":
             args.learningRate,
             args.trainableLayers
         )
+
     elif args.trainEfficientNet:
         efficientnet_model_object = EfficientNetWithAttention()
         efficientnet_model_object.train(
@@ -343,6 +501,7 @@ if __name__ == "__main__":
             args.learningRate,
             args.trainableLayers
         )
+
     elif args.test:
         resnext_object = ResNext()
         resnext_output = resnext_object.predict_videos(args.testVideoDirectory,
