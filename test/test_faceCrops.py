3a62af6e49c94dbb1834dbba20d4107ede4d2cea
Shahil Shah <shahilushah@gmail.com>
2021-03-09 18:53:38 -0500

    Update tests for generating face crops

--- test/test_faceCrops.py
+++ test/test_faceCrops.py
@@ -5,3 +5,6 @@ from os.path import isdir
 def test_face_crops():
     face_crops_dir = getcwd()+"/code/FaceCrops"
     assert isdir(face_crops_dir) == 1
+
+    face_crops_person_dir = getcwd()+"/code/FaceCrops/aaqaifqrwn/person1"
+    assert isdir(face_crops_person_dir) == 1
